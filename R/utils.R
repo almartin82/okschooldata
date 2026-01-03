8a0a94f077c6bcc1c32888abf2f67d3162a7eb17
almartin82 <almartin@gmail.com>
2026-01-02 20:47:49 -0500

    Update max_year to 2024 for confirmed data availability

--- R/utils.R
+++ R/utils.R
@@ -14,7 +14,7 @@ NULL
 #' @return A list with components:
 #'   \describe{
 #'     \item{min_year}{Earliest available school year end (2016)}
-#'     \item{max_year}{Latest available school year end (2025)}
+#'     \item{max_year}{Latest available school year end (2024)}
 #'     \item{description}{Human-readable description of the data availability}
 #'   }
 #' @export
@@ -26,7 +26,7 @@ NULL
 get_available_years <- function() {
   list(
     min_year = 2016L,
-    max_year = 2025L,
-    description = "Oklahoma enrollment data from OSDE is available for school years 2015-16 through 2024-25 (end years 2016-2025)"
+    max_year = 2024L,
+    description = "Oklahoma enrollment data from OSDE is available for school years 2015-16 through 2023-24 (end years 2016-2024)"
   )
 }
